61ebca41d0c14cd9bd4021192621254b90440103
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-04 01:13:13 +0530

    feat(US-003): add UserRepository

---
+++ UserService/src/main/java/com/dwellora/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.dwellora.repository;
+
+import com.dwellora.entity.User;
+import com.dwellora.enums.Role;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByApartmentIdAndRole(Integer apartmentId, Role role);
+
+    List<User> findByApartmentId(Integer apartmentId);
+
+    List<User> findByApartmentIdAndRole(Integer apartmentId, Role role);
+
+    List<User> findByRole(Role role);
+}
