399f1c7470b977faccd66bab19b6bcd271530cf3
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-04 12:16:32 +0530

    refactor: update UserService.java

--- UserService/src/main/java/com/dwellora/service/UserService.java
+++ UserService/src/main/java/com/dwellora/service/UserService.java
@@ -1,11 +1,8 @@
 package com.dwellora.service;
 
-import com.dwellora.dto.LoginRequestDTO;
-import com.dwellora.dto.LoginResponseDTO;
 import com.dwellora.dto.ManagerResponseDTO;
 import com.dwellora.dto.UserRequestDTO;
 import com.dwellora.dto.UserResponseDTO;
-import com.dwellora.dto.UserUpdateRequestDTO;
 import java.util.List;
 
 public interface UserService {
