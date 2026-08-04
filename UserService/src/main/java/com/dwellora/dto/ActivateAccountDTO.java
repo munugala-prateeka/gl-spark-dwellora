947996b6bac7d36b5122a9bd2a6b1211439e80e9
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-04 11:52:51 +0530

    feat(US-004): add ActivateAccountDTO

--- UserService/src/main/java/com/dwellora/dto/ActivateAccountDTO.java
+++ UserService/src/main/java/com/dwellora/dto/ActivateAccountDTO.java
@@ -1,4 +1,32 @@
 package com.dwellora.dto;
 
-public class ActivationAccountDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ActivateAccountDTO {
+
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String newPassword;
+
+    public ActivateAccountDTO() {}
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
