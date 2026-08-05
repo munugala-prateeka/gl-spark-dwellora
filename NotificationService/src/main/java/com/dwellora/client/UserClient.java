9febea36833c97b899eb7b8f56c675a41d5e7b10
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 19:30:50 +0530

    feat(US-015): add UserClient Feign interface to fetch apartment residents

--- NotificationService/src/main/java/com/dwellora/client/UserClient.java
+++ NotificationService/src/main/java/com/dwellora/client/UserClient.java
@@ -1,4 +1,15 @@
 package com.dwellora.client;
 
+import com.dwellora.dto.UserResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "USER-SERVICE")
 public interface UserClient {
+
+    @GetMapping("/users/apartment/{apartmentId}/residents")
+    List<UserResponseDTO> getResidentsByApartment(@PathVariable("apartmentId") Integer apartmentId);
 }
