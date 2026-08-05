190962201b5d055ebfba360fc469eef8909295dc
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 19:30:07 +0530

    feat(US-015): implement NoticeService layer with Kafka publishing

--- NoticeService/src/main/java/com/dwellora/service/NoticeService.java
+++ NoticeService/src/main/java/com/dwellora/service/NoticeService.java
@@ -1,4 +1,17 @@
 package com.dwellora.service;
 
+import com.dwellora.dto.NoticeRequestDTO;
+import com.dwellora.dto.NoticeResponseDTO;
+
+import java.util.List;
+
 public interface NoticeService {
+
+    NoticeResponseDTO publishNotice(NoticeRequestDTO request);
+
+    List<NoticeResponseDTO> getActiveNotices(Integer apartmentId);
+
+    NoticeResponseDTO getNoticeById(Integer noticeId);
+
+    void deleteNotice(Integer noticeId);
 }
