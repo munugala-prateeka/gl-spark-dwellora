9f1749482b55f559b91392e6d87e5985eeaf5d17
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 15:45:04 +0530

    feat(US-011): update LoggingAspect execution pointcuts

--- MaintenanceService/src/main/java/com/dwellora/utility/LoggingAspect.java
+++ MaintenanceService/src/main/java/com/dwellora/utility/LoggingAspect.java
@@ -1,4 +1,28 @@
 package com.dwellora.utility;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+    private final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.dwellora.service.impl.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        logger.info("Executing Method : {}", joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.dwellora.service.impl.*.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        logger.info("Completed Method : {}", joinPoint.getSignature().getName());
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.dwellora.service.impl.*.*(..))", throwing = "error")
+    public void logAfterThrowing(JoinPoint joinPoint, Exception error) {
+        logger.error("Exception in {} : {}", joinPoint.getSignature().getName(), error.getMessage());
+    }
 }
