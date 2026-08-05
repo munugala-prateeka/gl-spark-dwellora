d8ba6fb102cbfa0ed31c0acf55de0b9c594214b4
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 11:55:59 +0530

    feat(US-008): add line for logging

--- BookingService/src/main/java/com/dwellora/kafka/BookingProducer.java
+++ BookingService/src/main/java/com/dwellora/kafka/BookingProducer.java
@@ -1,12 +1,15 @@
 package com.dwellora.kafka;
 
 import com.dwellora.event.BookingCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingProducer {
 
+    private static final Logger logger = LoggerFactory.getLogger(BookingProducer.class);
     private static final String TOPIC = "booking-created";
 
     private final KafkaTemplate<String, BookingCreatedEvent> kafkaTemplate;
@@ -25,7 +28,7 @@ public class BookingProducer {
                 event
         );
 
-        System.out.println(
+        logger.info(
                 "Published Booking Event : "
                         + event.getBookingId());
 
