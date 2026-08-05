a5f23ad80b785da10ffd07cba76ca040c2e71cb8
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 17:20:04 +0530

    feat(US-012): add consumer config for complaint creation events

--- NotificationService/src/main/java/com/dwellora/config/KafkaComplaintCreatedConsumerConfig.java
+++ NotificationService/src/main/java/com/dwellora/config/KafkaComplaintCreatedConsumerConfig.java
@@ -1,4 +1,42 @@
 package com.dwellora.config;
 
-public class KafkaComplaintCreatedConfig {
+import com.dwellora.event.ComplaintCreatedEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaComplaintCreatedConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, ComplaintCreatedEvent> complaintCreatedConsumerFactory() {
+        JsonDeserializer<ComplaintCreatedEvent> deserializer =
+                new JsonDeserializer<>(ComplaintCreatedEvent.class);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeHeaders(false);
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
+
+    @Bean(name = "complaintCreatedKafkaListenerContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory<String, ComplaintCreatedEvent>
+    complaintCreatedKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, ComplaintCreatedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(complaintCreatedConsumerFactory());
+        return factory;
+    }
 }
