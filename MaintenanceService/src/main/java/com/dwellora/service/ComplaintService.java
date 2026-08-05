4e4aa708f976eccb40d0fba0f61949439b89aaa9
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 15:42:46 +0530

    feat(US-011): update ComplaintService method definitions

--- MaintenanceService/src/main/java/com/dwellora/service/ComplaintService.java
+++ MaintenanceService/src/main/java/com/dwellora/service/ComplaintService.java
@@ -2,7 +2,7 @@ package com.dwellora.service;
 
 import com.dwellora.dto.ComplaintRequestDTO;
 import com.dwellora.dto.ComplaintResponseDTO;
-import com.dwellora.dto.ComplaintUpdateDTO;
+
 import java.util.List;
 
 public interface ComplaintService {
@@ -12,6 +12,4 @@ public interface ComplaintService {
     List<ComplaintResponseDTO> getComplaintsByUser(Integer userId);
 
     List<ComplaintResponseDTO> getComplaintsByApartment(Integer apartmentId);
-
-    ComplaintResponseDTO updateComplaint(Integer complaintId, ComplaintUpdateDTO request);
 }
