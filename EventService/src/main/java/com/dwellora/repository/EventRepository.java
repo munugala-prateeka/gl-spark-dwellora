2156dd516e411caad74d3e1f27fce1a4f403a6a2
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 21:17:06 +0530

    feat(US-016): add EventRepository with upcoming events query

--- EventService/src/main/java/com/dwellora/repository/EventRepository.java
+++ EventService/src/main/java/com/dwellora/repository/EventRepository.java
@@ -1,4 +1,13 @@
 package com.dwellora.repository;
 
-public interface EventRepository {
+import com.dwellora.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Integer> {
+    List<Event> findByApartmentIdAndEventDateAfterOrderByEventDateAsc(Integer apartmentId, LocalDateTime now);
 }
