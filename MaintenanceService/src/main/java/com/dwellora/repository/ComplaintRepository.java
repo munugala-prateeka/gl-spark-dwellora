27a8904392721d690721e98d778fedcbac7cec30
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 15:42:43 +0530

    feat(US-011): update ComplaintRepository custom query methods

--- MaintenanceService/src/main/java/com/dwellora/repository/ComplaintRepository.java
+++ MaintenanceService/src/main/java/com/dwellora/repository/ComplaintRepository.java
@@ -1,4 +1,15 @@
 package com.dwellora.repository;
 
-public class ComplaintRepository {
+import com.dwellora.entity.Complaint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ComplaintRepository extends JpaRepository<Complaint, Integer> {
+
+    List<Complaint> findByUserId(Integer userId);
+
+    List<Complaint> findByApartmentIdOrderByRaisedAtDesc(Integer apartmentId);
 }
