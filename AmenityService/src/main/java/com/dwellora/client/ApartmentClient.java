52600dcc35672ed7d594c34b5f1e709ae713970b
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 09:27:10 +0530

    feat(US-007): add ApartmentClient Feign integration

---
+++ AmenityService/src/main/java/com/dwellora/client/ApartmentClient.java
@@ -0,0 +1,12 @@
+package com.dwellora.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "APARTMENT-SERVICE")
+public interface ApartmentClient {
+
+    @GetMapping("/apartments/{id}")
+    Object getApartmentById(@PathVariable Integer id);
+}
