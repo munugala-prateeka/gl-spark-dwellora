b46c89b6324b22bf40052946979bee480d85c58b
munugala-prateeka <munugala.prateeka@globallogic.com>
2026-08-05 21:46:49 +0530

    feat(US-017): add RsvpRepository for registration lookups

--- EventService/src/main/java/com/dwellora/repository/RsvpRepository.java
+++ EventService/src/main/java/com/dwellora/repository/RsvpRepository.java
@@ -1,4 +1,13 @@
 package com.dwellora.repository;
 
-public class ff {
+import com.dwellora.entity.Rsvp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RsvpRepository extends JpaRepository<Rsvp, Integer> {
+    boolean existsByEventIdAndResidentId(Integer eventId, Integer residentId);
+    Optional<Rsvp> findByEventIdAndResidentId(Integer eventId, Integer residentId);
 }
